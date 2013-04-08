a1055b72916788971388158bcb38106a49172957
Amitav Roy <reachme@amitavroy.com>
2013-04-08 12:38:44 +0530

    Global helper should autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('carabiner');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('url');
+$autoload['helper'] = array('url','global');
 
 
 /*
